19f3f1b382dfb532a1fd337f5bec563cb2756e4a
Eshu tomar <eshutomar1@gmial.com>
2024-08-10 14:56:21 +0530

    second problem

---
+++ AreaOfRect.java
@@ -0,0 +1,16 @@
+package DSA;
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class AreaOfRect {
+    public static void main(String[] args) {
+        	Scanner in = new Scanner(System.in);
+		
+		int length = in.nextInt();
+		int breadth = in.nextInt(); 
+
+		int area = length * breadth;
+		
+		System.out.println(area);
+    }
+}
